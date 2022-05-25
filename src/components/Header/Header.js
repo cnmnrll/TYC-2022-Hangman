d4d5f5defeee91719b27fb5e926182de27ec904d
timothxy <92201555+timothxy@users.noreply.github.com>
2022-05-25 16:07:34 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,13 @@ import React from "react";
 import "./Headers.css";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hangman</h1>
+      <h2>TikTok Youth Camp</h2>
+      <p>Guess the word - Key letter</p>
+    </div>;
+  )
 };
 
 export default Header;
