5f36892517be5e397ddc8e896ad37bbadea006e7
timothxy <92201555+timothxy@users.noreply.github.com>
2022-05-25 16:16:47 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from './components/Header';
+import DisplayWord from './components/DisplayWord';
 
-export default function app() {
-  return <div></div>;
+const words = ['apple', 'pear', 'orange', 'grape'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [rightLetters, setRightLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!rightLetters.includes(letter)) {
+            setRightLetters(currentLetters => [...currentLetters, letter]);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [rightLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setRightLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} rightLetters={correctLetters} />
+      </div>
+      <Popup rightLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+    </>
+  );
 }
+
+export default App;
+
+/*export default function app() {
+  return <div></div>;
+}*/
