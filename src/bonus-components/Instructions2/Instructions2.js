b8ac8cd6e4de3b07ea79fd215f70485f06aff61c
zhiyingsoh <105800646+zhiyingsoh@users.noreply.github.com>
2022-05-26 00:03:05 +0800

    Update Instructions2.js

--- src/bonus-components/Instructions2/Instructions2.js
+++ src/bonus-components/Instructions2/Instructions2.js
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 const Instructions2 = () => {
   return (
     <div>
-      <h1>Guess the word!<br/> Every wrong letter creates a new body part.<br/> Once the hangman is completed, you lose.<br/> If you guess the correct word before the hangman is completed, you win! </h1>
+      <h1>Instructions </h1>
+      <p>1. The player will first be given a category, e.g. animals.
+        <br/> 2. The player will then have to guess the word based on the category.
+        <br/> 3. If the player guesses the word correctly, he/she wins.
+        <br/> 4. If the player fails to guess the word by exceeding 10 wrong attempts, the man is hanged and he/she loses.
+      </p>
       <Link to="/Game2">
         <Button>START GAME</Button>
       </Link>
