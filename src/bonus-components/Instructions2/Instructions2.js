6ee291f6b078b7975be6d50213e9e740f35eb739
zhiyingsoh <105800646+zhiyingsoh@users.noreply.github.com>
2022-05-25 23:54:18 +0800

    Update Instructions2.js

--- src/bonus-components/Instructions2/Instructions2.js
+++ src/bonus-components/Instructions2/Instructions2.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Instructions2 = () => {
   return (
     <div>
-      <h1>Guess the word by pressing on the letters of the alphabat.<br/> For every wrong letter, the hangman loses a body part.<br/> Once the hangman has no more body parts, the game ends and you lose.<br/> If you guess the correct word, you win! </h1>
+      <h1>Guess the word!<br/> Every wrong letter creates a new body part.<br/> Once the hangman is completed, you lose.<br/> If you guess the correct word before the hangman is completed, you win! </h1>
       <Link to="/Game2">
         <Button>START GAME</Button>
       </Link>
