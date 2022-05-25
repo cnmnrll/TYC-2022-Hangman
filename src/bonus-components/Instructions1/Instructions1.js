cd6fb40747661b240171763e986d0e95fcb23029
timothxy <92201555+timothxy@users.noreply.github.com>
2022-05-25 23:49:07 +0800

    Update Instructions1.js

--- src/bonus-components/Instructions1/Instructions1.js
+++ src/bonus-components/Instructions1/Instructions1.js
@@ -6,6 +6,11 @@ const Instructions1 = () => {
   return (
     <div>
       <h1>Instructions1 goes here</h1>
+      <p>1. The player will first be given a category, e.g. animals.
+        <br/> 2. The player will then have to guess the word based on the category.
+        <br/> 3. If the player guesses the word correctly, he/she wins.
+        <br/> 4. If the player fails to guess the word by exceeding 10 wrong attempts, the man is hanged and he/she loses.
+      </p>
       <Link to="/Game1">
         <Button>START GAME</Button>
       </Link>
