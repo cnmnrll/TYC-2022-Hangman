26cf6933d82ddfac5068033454c44110dcd32262
zhiyingsoh <105800646+zhiyingsoh@users.noreply.github.com>
2022-05-26 00:08:27 +0800

    Update Instructions1.js

--- src/bonus-components/Instructions1/Instructions1.js
+++ src/bonus-components/Instructions1/Instructions1.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Instructions1 = () => {
   return (
     <div>
-      <h1>Instructions1 goes here</h1>
-      <p>1. The player will first be given a category, e.g. animals.
-        <br/> 2. The player will then have to guess the word based on the category.
-        <br/> 3. If the player guesses the word correctly, he/she wins.
-        <br/> 4. If the player fails to guess the word by exceeding 10 wrong attempts, the man is hanged and he/she loses.
+      <h1>Instructions</h1>
+      <p>1. Player will choose a category and input a word for other player(s) to guess.
+        <br/> 2. Other player(s) will then have to guess the word based on the cateogry hint.
+        <br/> 3. If the other player(s) guesses the word correctly, he/she wins.
+        <br/> 4. If the other player(s) fails to guess the word by exceeding 10 wrong attempts, the man is hanged and he/she loses.
       </p>
       <Link to="/Game1">
         <Button>START GAME</Button>
