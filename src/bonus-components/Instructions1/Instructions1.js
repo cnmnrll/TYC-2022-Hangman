dc3d1602843cf1f17763357d1e19b560ca5faee4
zhiyingsoh <105800646+zhiyingsoh@users.noreply.github.com>
2022-05-26 00:09:13 +0800

    Update Instructions1.js

--- src/bonus-components/Instructions1/Instructions1.js
+++ src/bonus-components/Instructions1/Instructions1.js
@@ -6,7 +6,7 @@ const Instructions1 = () => {
   return (
     <div>
       <h1>Instructions</h1>
-      <p>1. Player will choose a category and input a word for other player(s) to guess.
+      <p>1. Player will input a category and a word for other player(s) to guess.
         <br/> 2. Other player(s) will then have to guess the word based on the cateogry hint.
         <br/> 3. If the other player(s) guesses the word correctly, he/she wins.
         <br/> 4. If the other player(s) fails to guess the word by exceeding 10 wrong attempts, the man is hanged and he/she loses.
